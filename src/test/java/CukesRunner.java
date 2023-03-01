f57e571ef95226cedf4d7cced57a8ef30ac31f30
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-03-02 00:21:55 +0300

    hi

--- src/test/java/CukesRunner.java
+++ src/test/java/CukesRunner.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "html:cucumber-report.html",
+        plugin = {"html:cucumber-report.html", "json:target/cucumber.json", "junit:target/junit/junit-report.xml"},
         features = "src/test/resources/features",
         glue = "steps",
-        tags = "@wip"
+        tags = "@wip",
+        publish = true
 )
 
 public class CukesRunner {
