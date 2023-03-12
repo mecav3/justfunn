dfa4af1b4b098f13ae5516a855ba78950aa92a58
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-03-12 22:56:19 +0300

    employee

---
+++ truncate.sql
@@ -0,0 +1,19 @@
+truncate table invoice_products restart identity cascade;
+
+truncate table invoices restart identity cascade;
+
+truncate table clients_vendors restart identity cascade;
+
+truncate table payments restart identity cascade;
+
+truncate table products restart identity cascade;
+
+truncate table categories restart identity cascade;
+
+truncate table users restart identity cascade;
+
+truncate table companies restart identity cascade;
+
+truncate table addresses restart identity cascade;
+
+truncate table roles restart identity cascade;
